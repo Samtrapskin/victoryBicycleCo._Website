e2345073154940903007ae4a22b4069e4652508c
Sam Trapskin <sam.trapskin@totalexpert.com>
2019-02-07 16:52:34 -0600

    -added Boootstrap(4)-completed first section structure.

---
+++ jquery.js
@@ -0,0 +1,64 @@
+console.log('js');
+
+
+
+$( document ).ready(readyNow)
+
+function readyNow() {
+    console.log( 'jq');
+
+    $('.location').mouseenter (locMouseEnter);
+    $('.location').mouseleave (locMouseLeave);
+
+    $('.resources').mouseenter (locMouseEnter);
+    $('.resources').mouseleave (locMouseLeave);
+
+    $('.shop').mouseenter (shopMouseEnter);
+    $('.shop').mouseleave (shopMouseLeave);
+
+    $('.contact').mouseenter (contactMouseEnter);
+    $('.contact').mouseleave (contactMouseLeave);
+}
+
+
+
+
+
+// nav mouse enter
+function locMouseEnter (e) {
+    console.log('now red');
+    $(e.target).css('background-color', 'red');
+}
+function resourcesMouseEnter (e) {
+    console.log('now red');
+    $(e.target).css('background-color', 'red');
+}
+function contactMouseEnter (e) {
+    console.log('now red');
+    $(e.target).css('background-color', 'red');
+}
+function shopMouseEnter (e) {
+    console.log('now red');
+    $(e.target).css('background-color', 'red');
+}
+
+   //nav mouse leave
+function locMouseLeave (e) {
+    console.log('now red');
+    $(e.target).css('background-color', 'black');
+}
+
+function resourcesMouseLeave (e) {
+    console.log('now red');
+    $(e.target).css('background-color', 'black');
+}
+
+function shopMouseLeave (e) {
+    console.log('now red');
+    $(e.target).css('background-color', 'black');
+}
+
+function contactMouseLeave (e) {
+    console.log('now red');
+    $(e.target).css('background-color', 'black');
+}
