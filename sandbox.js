f8281448f5e258ef40b43aaa9a3b11c97c4b6f55
Sam Trapskin <sam.trapskin@totalexpert.com>
2019-03-19 13:23:58 -0500

    Removed shop section added to shop link in nav/navbar coomplete

---
+++ sandbox.js
@@ -0,0 +1,75 @@
+
+
+         
+         
+            <div class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+              <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-1"></div>
+
+                  <div class="col-lg-2">
+                    <div class="link-header">
+                        <a class="header-click" href="#"><p class="dropdown-header">Bikes</p></a>
+                    </div>
+                    <ul class="shop-categories">
+                      <li><a class="dropdown-item" href="#">Commuter/Urban</a></li>
+                      <li><a class="dropdown-item" href="#">Hybrid</a></li>
+                      <li><a class="dropdown-item" href="#">Mountain</a></li>
+                      <li><a class="dropdown-item" href="#">Childrens</a></li>
+                      <li><a class="dropdown-item" href="#">E-bikes</a></li>
+                      <li><a class="dropdown-item" href="#">Clearance</a></li>
+                    </ul>
+                  </div>
+                  <div class="col-lg-1"></div>
+
+                  <div class="col-lg-2">
+                    <div class="link-header">
+                      <p class="dropdown-header">Accessories</p>
+                    </div>
+                    <ul class="shop-categories">
+                      <li><a class="dropdown-item" href="#">Child carriers</a></li>
+                      <li><a class="dropdown-item" href="#">Safety</a></li>
+                      <li><a class="dropdown-item" href="#">Clothing</a></li>
+                      <li><a class="dropdown-item" href="#">Electronics</a></li>
+                      <li><a class="dropdown-item" href="#">Lights</a></li>
+                      <li><a class="dropdown-item" href="#">Locks/Security</a></li>
+                      <li><a class="dropdown-item" href="#">Pumps/Inflation</a></li>
+                      <li><a class="dropdown-item" href="#">Tools</a></li>
+                    </ul>
+                  </div>
+                  <div class="col-lg-1"></div>
+                  <div class="col-lg-3">
+                    <div class="link-header">
+                      <p class="dropdown-header">Parts and Components</p>
+                    </div>
+                    <ul class="shop-categories">
+                      <li><a class="dropdown-item" href="#">Tubes and Tires</a></li>
+                      <li><a class="dropdown-item" href="#">Seats and Sanddles</a></li>
+                      <li><a class="dropdown-item" href="#">Brakes</a></li>
+                      <li><a class="dropdown-item" href="#">Shifters</a></li>
+                      <li><a class="dropdown-item" href="#">Cables</a></li>
+                      <li><a class="dropdown-item" href="#">Suspension</a></li>
+                      <li><a class="dropdown-item" href="#">Other</a></li>
+                    </ul>
+                  </div>
+                </div>
+              
+            </div>
+
+
+
+
+
+            <div class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+              <a class="dropdown-item" href="#">Main</a>
+              <a class="dropdown-item" href="#">Tires and Tubes</a>
+              <a class="dropdown-item" href="#">Seats and Saddles</a>
+              <a class="dropdown-item" href="#">Brakes</a>
+              <a class="dropdown-item" href="#">Handlebars</a>
+              <a class="dropdown-item" href="#">Shifters</a>
+              <a class="dropdown-item" href="#">Cables</a>
+              <a class="dropdown-item" href="#">Suspension</a>
+              <a class="dropdown-item" href="#">Other</a>
+            </div>
+          </div>
+
